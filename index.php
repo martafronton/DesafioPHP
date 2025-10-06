e4a89af3ebc56a1daea48ca2cbdce52410be158b
martafronton <martafronton@gmail.com>
2025-10-06 07:04:20 +0200

    Funcionalidad registrar usuario
    El administrador puede registrar usuarios y obtener los usuarios registrados

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once("./Controllers/administradorController.php");
+
+$adminCtrl = new AdministradorController();
+$parametros = explode("/", $_SERVER["REQUEST_URI"]);
+
+unset($parametros[0]);
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $adminCtrl->insertarUsuario();
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    if (count($parametros) == 1 && $parametros[1] === "usuarios") { 
+        $adminCtrl->mostrarUsuarios();
+    } elseif (count($parametros) == 2 && $parametros[1] === "usuarios") {
+        $adminCtrl->mostrarUsuario($parametros[2]);
+    } else {
+        echo json_encode(["error" => "Ruta no encontrada"]);
+    }
+}
+
+
